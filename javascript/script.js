ebc999c6fc2660a09b1fa799caed6aa06020f880
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-04-19 22:27:31 -0400

    Rewriting JavaScript

--- javascript/script.js
+++ javascript/script.js
@@ -7,7 +7,7 @@ const quizQuestions = document.querySelector('#quiz-questions');
 const quizGame = document.querySelector('#quiz-game');
 const resultBox = document.querySelector('#result-box');
 
-const questionContent = document.querySelector('#question')
+const questionContent = document.querySelector('#question-text')
 // So we can dynamically change questions content;
 
 const option1 = document.querySelector('#option1');
@@ -16,13 +16,13 @@ const option3 = document.querySelector('#option3');
 const option4 = document.querySelector('#option4');
 // To dynamically change answer options content;
 
-const btn1 = document.querySelector('#btn1');
-const btn2 = document.querySelector('#btn2');
-const btn3 = document.querySelector('#btn3');
-const btn4 = document.querySelector('#btn4');
+const btn1 = document.querySelectorAll('#btn1');
+// const btn2 = document.querySelector('#btn2');
+// const btn3 = document.querySelector('#btn3');
+// const btn4 = document.querySelector('#btn4');
 // When we choose the option, this option will have the color, and if a palyer changes their answer option, color changes 
 
-const btn = document.querySelector('#btn-next');
+const btnNext = document.querySelector('#btn-next');
 // To be able to change question once button clicked;
 
 const questionNumber = document.querySelector('#number-question');
@@ -36,32 +36,112 @@ const resultContent = document.querySelector('#result');
 const btnPlayAgain = document.querySelector('#play-again-btn');
 // // To be able to play again once button clicked;
 
-let index = 0;
-let timer = 0;
-let interval = 0;
+let questionCounter = 0;
+let currentQuestion;
+let avaibleQuestions = [];
 
-// total score
-let correct = 0;
 
-// store answer value
-let userAnswer = undefined;
+// We push questions from _questionsBank_ into _avaibleQuestions_ array
+function setAvaibleQuestions() {
+const totalQuestions = questionsBank.length;
+for (let i = 0; i < totalQuestions; i++) {
+  avaibleQuestions.push(questionsBank[i]);
+}
+}
+
+// Set question number and question options
+function getNewQuestion() {
+  // Set question number
+  questionNumber.innerHTML = `Question ${questionCounter + 1} of 10`;
+
+  // Set question text
+  // Get random question
+  const questionIndex = avaibleQuestions[Math.floor(Math.random() * avaibleQuestions.length)];
+  currentQuestion = questionIndex;
+  questionContent.textContent = currentQuestion.question;
+  
+  // Set options text
+  option1.textContent = currentQuestion.option1;
+  option2.textContent = currentQuestion.option2;
+  option3.textContent = currentQuestion.option3;
+  option4.textContent = currentQuestion.option4;
+
+
+}
+
+window.onload = function() {
+  // _Lets Start_ button is clicked, change to the _game quiz_ screen
+  startButton.addEventListener('click', () => {
+    startScreen.style.display = 'none';
+    body.style.backgroundImage = 'url(pikachu.jpg)';
+    quizGame.style.display = 'block';
+  });
+
+
+  // 1. We set all questions in _avaibleQuestions_ array 
+  setAvaibleQuestions();
+//  2. We call _getNewQuestion()_ function
+  getNewQuestion();
+}
+
+
+
+
+
+
 
 // When start button of the Start screen clicked, change to the Game screen + background
-startButton.addEventListener('click', () => {
-  startScreen.style.display = 'none';
-  body.style.backgroundImage = 'url(pikachu.jpg)';
-  quizGame.style.display = 'block';
+// startButton.addEventListener('click', () => {
+//   startScreen.style.display = 'none';
+//   body.style.backgroundImage = 'url(pikachu.jpg)';
+//   quizGame.style.display = 'block';
 
-});
+// });
+
+// // Function to load questions
+// function loadQuestions(index) {
+//   questionContent.textContent = `${index + 1}. ${questionsBank[index].question}`;
+//   option1.textContent = questionsBank[index].option1;
+//   option2.textContent = questionsBank[index].option2;
+//   option3.textContent = questionsBank[index].option3;
+//   option4.textContent = questionsBank[index].option4;
+// }
+
+// loadQuestions(4);
+
+// // Function when next button is clicked
+// btnNext.addEventListener('click', () => {
+//   quizGame.style.display = 'block';
+//   loadQuestions();
+
+// })
 
 
-function loadQuestions(index) {
-  questionContent.textContent = questionsBank[index].question;
-  option1.textContent = questionsBank[index].option1;
-  option2.textContent = questionsBank[index].option2;
-  option3.textContent = questionsBank[index].option3;
-  option4.textContent = questionsBank[index].option4;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+// function getRandomQuestionSet(amountofQuestions) {
+//   let qSet = [];
+
+//   while(qSet.length < amountofQuestions.length) {
+//     let randomIndex = Math.floor(Math.random() * questionsBank.length);
+    
+//     if (qSet.indexOf(randomIndex) === -1) {
+//       qSet.push(randomIndex);
+//     }
+//   }
+//   return questionsBank.filter( (element, ind) => qSet.indexOf(ind) > -1);
+// }
+
 
 // function generateRandomQuestions() {
 //   const randomNumber = Math.floor(Math.random() * questionsBank.length);
