e5caf2415d491230bb9bf609644eef66698852ae
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-04-24 09:43:18 -0400

    Keep working with classes

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -1,168 +1,86 @@
-class QuizGame {
-constructor(questions) {
-  this.body = document.querySelector('body');
-  this.startScreen = document.querySelector('.start-container');
-  this.startButton = document.querySelector('.btn');
-  this.game = document.querySelector('#quiz-game');
-  this.quizQuestions = document.querySelector('#quiz-questions');
-  this.resultBox = document.querySelector('#result-box');
-  this.questionContent = document.querySelector('#question-text');
+class Quiz {
+  constructor(questions) {
+    this.questions = questions;
+    this.score = 0;
+    this.currentQuestionIndex = 0;
+    this.progressTrack;
+  }
 
-  this.questionNumber = document.querySelector('#number-question');
+  getCurrentQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
+  getNextQuestion() {
+    this.currentQuestionIndex++;
+    return this.getCurrentQuestion();
+  }
 
-  this.option1 = document.querySelector('#option1');
-  this.option2 = document.querySelector('#option2');
-  this.option3 = document.querySelector('#option3');
-  this.option4 = document.querySelector('#option4');
-  this.btn1 = document.querySelector('#btn1');
-  this.btn2 = document.querySelector('#btn2');
-  this.btn3 = document.querySelector('#btn3');
-  this.btn4 = document.querySelector('#btn4');
+  checkAnswer(answer) {
+    const currentQuestion = this.getCurrentQuestion();
+    if (answer === currentQuestion.answer) {
+      this.score++;
+      return true;
+    }
+    return false;
+  }
 
-
-
-  this.score = 0;
-  this.questions = questions;
-  this.currentQuestion;
-  this.questionCounter = 0;
-  this.randomQuestionsArray = [];
-  this.progressTrack;
-  this.userAnswer;
+  isGameOver() {
+    return this.currentQuestionIndex >= this.questions.length;
+  }
 }
 
-setAvaibleQuestions(questions) {
-  const shuffledAr = [...questions];
-for (let i = shuffledAr.length - 1; i > 0; i--) {
-  const j = Math.floor(Math.random() * (i + 1));
-  [shuffledAr[i], shuffledAr[j]] = [shuffledAr[j], shuffledAr[i]];
-}
-return this.randomQuestionsArray.push(shuffledAr);
-}
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.score = 0;
+  }
 
-// answerCurrentQuestion(option) {
-//   const currentQuestion = this.getCurrentQuestion();
-//   if (currentQuestion.answer === option) {
-//     this.score++;
-//   }
-// }
-
-getNewQuestion() {
-  // this.questionNumber.textContent = `Question ${this.questionCounter + 1} of 10`;
-
-  const randomQuestionObj = this.randomQuestionsArray[Math.floor(Math.random() * this.randomQuestionsArray.length)];
-  this.currentQuestion = randomQuestionObj.question;
-  this.questionContent.textContent = this.currentQuestion;
-
-  this.option1.textContent = this.randomQuestionObj.option1;
-  this.option2.textContent = this.randomQuestionObj.option2;
-  this.option3.textContent = this.randomQuestionObj.option3;
-  this.option4.textContent = this.randomQuestionObj.option4;
-
-  this.questionCounter++;
-
+  updateScore(score) {
+    this.score += score;
+  }
 }
 
-startGame() {
-  this.startButton.addEventListener('click', () => {
-    this.startScreen.style.display = 'none';
-    this.body.style.backgroundImage = 'url(pikachu.jpg)';
-    this.game.style.display = 'block';
-  });
-}
-
-}
-
-const quizGame = new QuizGame(questionsBank);
+const quiz = new Quiz(questionsBank);
+const player = new Player('Player 1');
 
 
 window.addEventListener('load', (event) => {
-  quizGame.startGame();
-});
+  const startButton = document.querySelector('.btn');
+  const startScreen = document.querySelector('.start-container');
+  const body = document.querySelector('body');
+  const quizGame = document.querySelector('#quiz-game');
+  startButton.addEventListener('click', () => {
+    startScreen.style.display = 'none';
+    body.style.backgroundImage = 'url(pikachu.jpg)';
+    quizGame.style.display = 'block';
+  })
+})
 
 function displayQuestion() {
-  quizGame.questionNumber.textContent = `Question ${quizGame.questionCounter + 1} of 10`;
+  if (quiz.isGameOver()) {
+    showScore();
+  } else {
+    const questionContent = document.querySelector('#question-text');
+    questionContent.innerHTML = quiz.getCurrentQuestion().question;
 
-  if (quizGame.questionCounter === 9) {
-    quizGame.btnNext.textContent = 'Check the score!';
-  } else if (quizGame.btn1.style.backgroundColor === 'rgb(253, 239, 165)' &&
-  quizGame.btn2.style.backgroundColor === 'rgb(253, 239, 165)' &&
-  quizGame.btn3.style.backgroundColor === 'rgb(253, 239, 165)' &&
-  quizGame.btn4.style.backgroundColor === 'rgb(253, 239, 165)') {
-    alert(`'Please select an option'`)
-    return;
+const option1 = document.querySelector('#option1');
+const option2 = document.querySelector('#option2');
+const option3 = document.querySelector('#option3');
+const option4 = document.querySelector('#option4');
+
+option1.innerHTML = quiz.getCurrentQuestion().option1;
+option2.innerHTML = quiz.getCurrentQuestion().option2;
+option3.innerHTML = quiz.getCurrentQuestion().option3;
+option4.innerHTML = quiz.getCurrentQuestion().option4;
   }
+  showProgress();
+}
 
-  if (quizGame.questionCounter > 9) {
-
-  }
-
-
+function showProgress() {
+  const currentQuestionNumber = quiz.currentQuestionIndex + 1;
+  const questionNumber = document.querySelector('#number-question');
+  questionNumber.innerHTML = `Question ${currentQuestionNumber} of ${quiz.questions.length}`;
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// window.onload = function() {
-//   // _Lets Start_ button is clicked, change to the _game quiz_ screen
-//   startButton.addEventListener('click', () => {
-//     startScreen.style.display = 'none';
-//     body.style.backgroundImage = 'url(pikachu.jpg)';
-//     quizGame.style.display = 'block';
-//   });
-
-//   btnHome.addEventListener('click', () => {
-//     quizGame.style.display = 'none';
-//     resultBox.style.display = 'none';
-//     body.style.backgroundImage = 'url(doraemon.jpg)';
-//     startScreen.style.display = 'flex';
-//     goHomeMenu();
-//   });
-
-//   // To make work _next question_ button
-//   btnNext.addEventListener('click', nextQuestion);
-
-//   //
-//   option1.addEventListener('click', getResult);
-//   option2.addEventListener('click', getResult);
-//   option3.addEventListener('click', getResult);
-//   option4.addEventListener('click', getResult);
-
-//   // 1. We set all questions in _avaibleQuestions_ array 
-//   setAvaibleQuestions();
-// //  2. We call _getNewQuestion()_ function to display questions and options
-//   getNewQuestion();
-// }
+displayQuestion();
--- javascript/scripttemp.js
+++ javascript/scripttemp.js
@@ -248,3 +248,498 @@ countDownId = setTimeout(startTimer, 1000);
 
 }
 // startTimer(seconds, timerD);
+
+// --------------------------------------------------
+// --------------------------------------------------
+// -------------------------------------------------
+
+class QuizGame {
+  constructor(questions) {
+    this.body = document.querySelector('body');
+    this.startScreen = document.querySelector('.start-container');
+    this.startButton = document.querySelector('.btn');
+    this.game = document.querySelector('#quiz-game');
+    this.quizQuestions = document.querySelector('#quiz-questions');
+    this.resultBox = document.querySelector('#result-box');
+    this.questionContent = document.querySelector('#question-text');
+  
+    this.questionNumber = document.querySelector('#number-question');
+  
+  
+    this.option1 = document.querySelector('#option1');
+    this.option2 = document.querySelector('#option2');
+    this.option3 = document.querySelector('#option3');
+    this.option4 = document.querySelector('#option4');
+    this.btn1 = document.querySelector('#btn1');
+    this.btn2 = document.querySelector('#btn2');
+    this.btn3 = document.querySelector('#btn3');
+    this.btn4 = document.querySelector('#btn4');
+  
+  
+  
+    this.score = 0;
+    this.questions = questions;
+    this.currentQuestionObj;
+    this.questionCounter = 0;
+    this.randomQuestionsArray = [];
+    this.progressTrack;
+    this.userAnswer;
+  }
+  
+  startGame() {
+    this.startButton.addEventListener('click', () => {
+      this.startScreen.style.display = 'none';
+      this.body.style.backgroundImage = 'url(pikachu.jpg)';
+      this.game.style.display = 'block';
+    });
+  
+    this.setQuestions();
+  }
+  
+  setAvaibleQuestions(questions) {
+    const shuffledAr = [...questions];
+  for (let i = shuffledAr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledAr[i], shuffledAr[j]] = [shuffledAr[j], shuffledAr[i]];
+  }
+  return this.randomQuestionsArray.push(shuffledAr);
+  }
+  
+  getNewQuestion() {
+    this.questionNumber.textContent = `Question ${this.questionCounter + 1} of 10`;
+  
+    const randomQuestionObj = this.randomQuestionsArray[Math.floor(Math.random() * this.randomQuestionsArray.length)];
+    this.currentQuestionObj = randomQuestionObj;
+    return this.currentQuestionObj;
+    this.currentQuestion = randomQuestionObj.question;
+    this.questionContent.textContent = this.currentQuestion;
+  
+    this.option1.textContent = this.randomQuestionObj.option1;
+    this.option2.textContent = this.randomQuestionObj.option2;
+    this.option3.textContent = this.randomQuestionObj.option3;
+    this.option4.textContent = this.randomQuestionObj.option4;
+  
+    this.questionCounter++;
+  }
+  
+  
+  }
+  
+  const quizGame = new QuizGame(questionsBank);
+  
+  
+  window.addEventListener('load', (event) => {
+    quizGame.startGame();
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // setAvaibleQuestions(questions) {
+  //   const shuffledAr = [...questions];
+  // for (let i = shuffledAr.length - 1; i > 0; i--) {
+  //   const j = Math.floor(Math.random() * (i + 1));
+  //   [shuffledAr[i], shuffledAr[j]] = [shuffledAr[j], shuffledAr[i]];
+  // }
+  // return this.randomQuestionsArray.push(shuffledAr);
+  // }
+  
+  // getNewQuestion() {
+  //   this.questionNumber.textContent = `Question ${this.questionCounter + 1} of 10`;
+  
+  //   const randomQuestionObj = this.randomQuestionsArray[Math.floor(Math.random() * this.randomQuestionsArray.length)];
+  //   this.currentQuestion = randomQuestionObj.question;
+  //   this.questionContent.textContent = this.currentQuestion;
+  
+  //   this.option1.textContent = this.randomQuestionObj.option1;
+  //   this.option2.textContent = this.randomQuestionObj.option2;
+  //   this.option3.textContent = this.randomQuestionObj.option3;
+  //   this.option4.textContent = this.randomQuestionObj.option4;
+  
+  //   this.questionCounter++;
+  // }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // function displayQuestion() {
+  //   quizGame.questionNumber.textContent = `Question ${quizGame.questionCounter + 1} of 10`;
+  
+  //   if (quizGame.questionCounter === 9) {
+  //     quizGame.btnNext.textContent = 'Check the score!';
+  //   } else if (quizGame.btn1.style.backgroundColor === 'rgb(253, 239, 165)' &&
+  //   quizGame.btn2.style.backgroundColor === 'rgb(253, 239, 165)' &&
+  //   quizGame.btn3.style.backgroundColor === 'rgb(253, 239, 165)' &&
+  //   quizGame.btn4.style.backgroundColor === 'rgb(253, 239, 165)') {
+  //     alert(`'Please select an option'`)
+  //     return;
+  //   }
+  
+  //   if (quizGame.questionCounter > 9) {
+  //     if (quizGame.userAnswer === quizGame.currentQuestion.answer) {
+  //       quizGame.setCorrect();
+  //     } else {
+  //       quizGame.setWrong();
+  //     }
+  //     quizGame.displayResultBox();
+  //     return
+  //   }
+  
+  
+  // }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // setAvaibleQuestions(questions) {
+  //   const shuffledAr = [...questions];
+  //   for (let i = 0; i < shuffledAr.length; i++) {
+  //     return randomQuestionsArray.push(shuffledAr[i]);
+  //   }
+  // }
+  
+  // getNewQuestion() {
+  //   this.questionNumber.textContent = `Question ${this.questionCounter + 1} of 10`;
+  
+  //   const questionIndex = this.randomQuestionsArray[Math.floor(Math.random() * this.randomQuestionsArray.length)];
+  //   this.currentQuestion = questionIndex;
+  //   this.questionContent.textContent = this.currentQuestion.question;
+  
+  //   this.option1.textContent = this.currentQuestion.option1;
+  //   this.option2.textContent = this.currentQuestion.option2;
+  //   this.option3.textContent = this.currentQuestion.option3;
+  //   this.option4.textContent = this.currentQuestion.option4;
+  
+  //   const index1 = this.randomQuestionsArray.indexOf(questionIndex);
+  //   this.randomQuestionsArray.splice(index1, 1);
+  
+  //   this.questionCounter++;
+  // }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // window.onload = function() {
+  //   // _Lets Start_ button is clicked, change to the _game quiz_ screen
+  //   startButton.addEventListener('click', () => {
+  //     startScreen.style.display = 'none';
+  //     body.style.backgroundImage = 'url(pikachu.jpg)';
+  //     quizGame.style.display = 'block';
+  //   });
+  
+  //   btnHome.addEventListener('click', () => {
+  //     quizGame.style.display = 'none';
+  //     resultBox.style.display = 'none';
+  //     body.style.backgroundImage = 'url(doraemon.jpg)';
+  //     startScreen.style.display = 'flex';
+  //     goHomeMenu();
+  //   });
+  
+  //   // To make work _next question_ button
+  //   btnNext.addEventListener('click', nextQuestion);
+  
+  //   //
+  //   option1.addEventListener('click', getResult);
+  //   option2.addEventListener('click', getResult);
+  //   option3.addEventListener('click', getResult);
+  //   option4.addEventListener('click', getResult);
+  
+  //   // 1. We set all questions in _avaibleQuestions_ array 
+  //   setAvaibleQuestions();
+  // //  2. We call _getNewQuestion()_ function to display questions and options
+  //   getNewQuestion();
+  // }
+
+
+// ---------------------------------
+//-------------------------------------
+//-------------------------------------
+//---------------------------------------
+//-------------------------------------------------
+class QuizGame {
+  constructor(questions) {
+    this.questions = questions;
+    this.currentQuestion = {};
+    this.score = 0;
+    this.questionIndex = 0;
+  }
+
+  // --- Generate a random question from questionsBank
+  getRandomQuestion() {
+    const randomIndex = Math.floor(Math.random() * this.questions.length);
+    this.currentQuestion = this.questions[randomIndex];
+    return this.questions.splice(randomIndex, 1);
+    //Removes selected question from the QuestionBank;
+  }
+
+  // --- Check if the answer is correct and update the score
+  annswerQuestion(answer) {
+    if (answer === this.currentQuestion.answer) {
+      this.score++;
+      return true;
+    } else {
+      return false;
+    }
+  } 
+
+  // --- Get the next question
+  getNextQuestion() {
+    this.getRandomQuestion();
+    this.questionIndex++;
+    return this.currentQuestion;
+  }
+
+  // --- Check if game is over
+  isGameOver() {
+    return this.currentQuestionIndex >= this.questions[9];
+  }
+}
+
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.score = 0;
+  }
+
+  updateScore() {
+    this.score += score;
+  }
+}
+
+const quiz = new QuizGame(questions);
+const player = new Player('Player 1');
+
+const body = document.querySelector('body');
+const startScreen = document.querySelector('.start-container');
+const startButton = document.querySelector('.btn');
+const quizQuestions = document.querySelector('#quiz-questions'); 
+const quizGame = document.querySelector('#quiz-game');
+const resultBox = document.querySelector('#result-box');
+
+const questionContent = document.querySelector('#question-text')
+// So we can dynamically change questions content;
+
+const option1 = document.querySelector('#option1');
+const option2 = document.querySelector('#option2');
+const option3 = document.querySelector('#option3');
+const option4 = document.querySelector('#option4');
+// To dynamically change answer options content;
+
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+const btn3 = document.querySelector('#btn3');
+const btn4 = document.querySelector('#btn4');
+// When we choose the option, this option will have the color, and if a palyer changes their answer option, color changes 
+
+const btnNext = document.querySelector('#btn-next');
+// To be able to change question once button clicked;
+
+const questionNumber = document.querySelector('#number-question');
+// To dynamically change question numbers and didplay them;
+
+function displayQuestion() {
+  const currentQuestion = quiz.getRandomQuestion();
+  questionContent.textContent = currentQuestion.question;
+  option1.textContent = currentQuestion.option1;
+  option2.textContent = currentQuestion.option2;
+  option3.textContent = currentQuestion.option3;
+  option4.textContent = currentQuestion.option4;
+
+
+}
+
+
+window.addEventListener('load', (event) => {
+  startButton.addEventListener('click', () => {
+    startScreen.style.display = 'none';
+    body.style.backgroundImage = 'url(pikachu.jpg)';
+    quizGame.style.display = 'block';
+  })
+
+  displayQuestion();
+})
+
+
+//-------------------------------------------------------
+//------------------------------------------------------
+//------------------------------------------------------
+// Create a quiz class
+class Quiz {
+  constructor(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+  }
+
+  getQuestionIndex() {
+    return this.questions[this.questionIndex];
+  }
+
+  guess(answer) {
+    if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+      this.score++;
+    }
+    this.questionIndex++;
+  }
+
+  isEnded() {
+    return this.questionIndex === this.questions.length;
+  }
+}
+
+// Create a question class
+
+class Question {
+  constructor(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+  }
+
+  isCorrectAnswer(choice) {
+    return this.answer === choice;
+  }
+}
+
+// Display question function
+
+function displayQuestion() {
+  if (quiz.isEnded()) {
+    showScore();
+  } else {
+    // show next question
+    let questionContent = document.querySelector('#question-text');
+    questionContent.innerHTML = quiz.getQuestionIndex().text;
+
+    // show options
+    // let choices = quiz.getQuestionIndex().choices;
+    // for (let i = 0; i < choices.length; i++) {
+    //   let choiceElement = document.getElementById(`option${i}`);
+    //   choiceElement.innerHTML = choices[i];
+    //   guess('btn' + i, choices[i]);
+    // }
+
+    showProgess();
+  }
+};
+
+// Guess function
+function guess(id, guess) {
+  let button = document.getElementById(id);
+  button.onclick = function() {
+    quiz.guess(guess);
+    displayQuestion();
+  }
+}
+
+// Show progress
+function showProgess() {
+  let currentQuestionNumber = quiz.questionIndex + 1;
+  let questionNumber = document.querySelector('#number-question');
+  questionNumber.innerHTML = `Question ${currentQuestionNumber} of ${quiz.questions.length}`;
+
+}
+
+// Show score function
+function showScore() {
+  let quizEndHTML = 
+  `
+  <h1>Quiz Completed</h1>
+  <h2>Your score is ${quiz.score}</h2>
+  `;
+
+  const quizGame = document.querySelector('#quiz-game');
+  quizGame.innerHTML = quizEndHTML;
+}
+
+// Create quiz questions
+let questions = [
+  new Question(
+    'Which Pokémon has electricity-storing pouches on its cheeks?',
+    ['Eevee', 'Ditto', 'Pikachu', 'Charmander'],
+    'Pikachu'
+),
+
+new Question(
+  'How many Dragon Balls are there?',
+  ['Seven', 'Five', 'Ten', 'Three'],
+  'Seven'
+),
+
+new Question(
+  'Which anime series revolves around a boy who sells his soul to a demon?',
+  ['Naruto', 'Black Butler', 'Bleach', 'Attack on Titan'],
+  'Black Butler'
+),
+
+];
+
+let quiz = new Quiz(questions);
+
+// display questions
+displayQuestion();
+
+window.addEventListener('load', (event) => {
+  const startButton = document.querySelector('.btn');
+  const startScreen = document.querySelector('.start-container');
+  const body = document.querySelector('body');
+  const quizGame = document.querySelector('#quiz-game');
+  startButton.addEventListener('click', () => {
+    startScreen.style.display = 'none';
+    body.style.backgroundImage = 'url(pikachu.jpg)';
+    quizGame.style.display = 'block';
+  })
+})
